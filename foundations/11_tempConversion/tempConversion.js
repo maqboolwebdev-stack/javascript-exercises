218473666705d37bdb73fb1824d26aabbb48217f
Waqas Maqbool <maqbool.webdev@gmail.com>
2026-03-12 18:33:14 +0500

    TempConversion solved

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+
 };
 
+console.log(convertToFahrenheit(34));
+console.log(convertToCelsius(80));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
