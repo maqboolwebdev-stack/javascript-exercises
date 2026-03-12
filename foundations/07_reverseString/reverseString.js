16c2fcb6de4e91bbb377c8b4eaadbdf2fd741c19
Waqas Maqbool <maqbool.webdev@gmail.com>
2026-03-12 18:15:28 +0500

    ReverseString solved

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  return string.split("").reverse().join("");
 
 };
+console.log(reverseString('I am Waqas'));
 
 // Do not edit below this line
 module.exports = reverseString;
