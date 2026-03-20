97da39bbb6d240b4760452bfba3ba6be8e41a86f
waqas <maqbool.webdev@gmail.com>
2026-03-20 23:53:01 +0500

    12_calculator Solved

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return console.log(a + b);
 };
 
-const subtract = function() {
-	
+add(3, 4);
+
+const subtract = function(a, b) {
+	return console.log(a - b);
 };
 
-const sum = function() {
-	
+subtract(5, 12)
+
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
+console.log(sum([1,2,3,4,5,6,7]));
 
+const multiply = function(array) {
+    return array.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+console.log(multiply([1,2,3,4,5,6,7]));
+
+const power = function(a, b) {
+	return console.log(a ** b);
 };
 
-const factorial = function() {
-	
+power(3,3)
+
+const factorial = function(n) {
+	let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
+
+console.log(factorial(6));
 
 // Do not edit below this line
 module.exports = {
