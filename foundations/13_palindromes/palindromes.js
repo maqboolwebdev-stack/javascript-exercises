a17d7a0a7f3a7f108edfe8236c3411b70c87c907
waqas <maqbool.webdev@gmail.com>
2026-03-21 16:34:51 +0500

    13-palindromes solved

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const palindromes = text.toLowerCase().split('').reverse().join('');
+    return palindromes == text ? true : false;
 };
+
+console.log(palindromes('race'));
+console.log(palindromes('1racecar1'));
+console.log(palindromes('maria'));
+console.log(palindromes('1wdcdw1'));
 
 // Do not edit below this line
 module.exports = palindromes;
