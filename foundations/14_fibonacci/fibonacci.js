a534093676e25bca7e732ffa65518fa57e49f7c6
waqas <maqbool.webdev@gmail.com>
2026-03-22 11:28:50 +0500

    14_fibonacci solved

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let firstNumber = 0;
+    let secondNumber = 1;
+    for(let i = 2; i <= number; i++){
+        let current = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = current;
+    }
+    return console.log(secondNumber);
+    
 };
 
+fibonacci(6);
 // Do not edit below this line
 module.exports = fibonacci;
