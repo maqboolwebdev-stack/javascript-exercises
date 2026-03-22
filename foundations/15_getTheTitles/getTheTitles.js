3b6ad8bb2222f511a415c238930c9657f3bf1238
waqas <maqbool.webdev@gmail.com>
2026-03-22 12:24:11 +0500

    15_getTheTitles solved

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map((book) => book.title);
 };
-
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+];
+getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
